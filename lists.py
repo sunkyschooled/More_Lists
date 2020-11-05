2f78077d064c87429e03310ef165ceb63916aa63
Colin Tinsman <tinsmancb@gmail.com>
2020-11-05 11:08:00 -0500

    Completed Assignment

--- lists.py
+++ lists.py
@@ -1,5 +1,31 @@
-def main():
+def sum_of_odd_nums(n):
     pass
+
+def caesar_cipher(message, key):
+    cipher = [ord(c) + key for c in message]
+    return bytes(cipher).decode('ascii')
+
+def fizzbuzz(n):
+    pass
+        
+
+def main():
+    print('Table of the sum for the first n odd numbers:')
+    print('n\tsum')
+    print('-'*16)
+    for n in range(10):
+        print('{}\t{}'.format(n, sum_of_odd_nums(n)))
+
+    print()
+
+    ciphertext = "Frpsxwhu#Vflhqfh#lv#qr#pruh#derxw#frpsxwhuv#wkdq#dvwurqrp|#lv#derxw#whohvfrshv1"
+    print(caesar_cipher(ciphertext, -3))
+
+    print()
+
+    for i in fizzbuzz(25):
+        print(i)
+
 
 if __name__ == '__main__':
     main()
